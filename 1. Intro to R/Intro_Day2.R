60f77742b1788aeebf887bcf7320c937c5c8148f
Haldre Rogers <haldrerogers@haldre-2015.eeob.iastate.edu>
2019-09-03 14:27:02 -0500

    Add rest of the filesf or today

---
+++ 1. Intro to R/Intro_Day2.R
@@ -0,0 +1,7 @@
+
+### now it's your turn #### 
+
+#read in spider.csv 
+#explore dataset -what is min, max, average for the total # of webs? 
+#create a vector describing the number of webs per meter
+#make a histogram of the total number of webs
--- 1. Intro to R/R_introduction.R
+++ 1. Intro to R/R_introduction.R
@@ -1,17 +1,42 @@
-# RWorkshopDay1.R
+# Intro to R Day 1
 
-# topics
+######## Topics #######
+# Getting help
+# Working directories
 # Using R as a calculator
 # Functions
-# Getting help
 # Downloading new packages
 # Creating data and variables
 # Preparing and loading data files into R
 # Getting to know your data
-# Discussing your data with your computer
+# Square brackets - like an address for a dataset
 # Manipulating parts of data tables
 
-#then try it yourself with a new dataset!
+# ----- Getting help --------
+# If you know a function name, you can use
+#   the question mark ? to open a help file
+
+?mean #opens help file 
+?t.test
+
+# Help files tell possible arguments
+#   and give examples at the end
+
+help.start()  # opens the links to online manuals and miscellaneous materials
+
+# Or, open help tab (at right) and type name in
+# Or, google it! (There are great R forums)
+
+# ----- Working Directories ---------
+# Determine your working directory
+
+getwd()
+
+# Set your working directory using setwd()
+#   or by using "Set as working director" in the "More"
+#   option in the "Files" tab on the right
+
+setwd('/Users/Haldre/Desktop/')
 
 
 # ----- Using R as a calculator ------
@@ -47,25 +72,9 @@ cos(pi)
 #   the 2*x as the function I want to graph and
 #   "from" and "to" as arguments to specify x axis length
 
+?curve #see what the help file says
+
 curve(2*x, from=0,to=8)
-
-
-
-
-
-# ----- Getting help ------
-
-# If you know a function name, you can use
-#   the question mark ? to open a help file
-
-?t.test
-
-# Help files tell possible arguments
-#   and give examples at the end
-
-# Or, open help tab (at right) and type name in
-# Or, google it! (There are great R forums)
-
 
 
 
@@ -89,57 +98,43 @@ library(lme4)
 # ----- Creating data and variables ------
 
 
-
 # Make a vector with concatenate, c()
 
-c(1,2,3,4,5)
+c(5, 7, 3, 14)
 
 # Or save this as something
 
-myvector <- c(1,2,3,4,5)
+dogAges <- c(5, 7, 3, 14)
 
 # Type the name to see it
 
-myvector
+dogAges
 
 # Perform functions on vectors
 
-mean(myvector)
+mean(dogAges)
 
-myvector2 <- myvector*2
-myvector2
+dogAges2 <- dogAges*2
+dogAges2
 
 # Combine vectors
 
-myvectors <- cbind(myvector, myvector2)
-myvectors
-
-
-
+dogAges <- cbind(dogAges, dogAges2)
+dogAges
 
 
 
 # ----- Preparing and loading data files into R ------
 
-# Determine your working directory
+#   Can use .csv or .txt files or excel files
 
-getwd()
+# Read file using read.csv, naming it something. Note that this must be in your 
+# working directory
 
-# Set your working directory using setwd()
-#   or by using "Set as working director" in the "More"
-#   option in the "Files" tab on the right
+spider <- read.csv("1. Intro to R/spider.csv", header = TRUE)
 
-setwd('/Users/Haldre/Desktop/')
-
-# R likes .csv or .txt files
-#   so use Excel to save as one of those
-
-# Read file using read.csv, naming it something
-
-data <- read.csv('RWorkshopDay1.csv', header=TRUE)
-
-# You can ignore the wd and use file.choose()
-# data <- read.csv(file.choose())
+# You can also use use file.choose()
+spider <- read.csv(file.choose())
 
 
 
@@ -150,40 +145,40 @@ data <- read.csv('RWorkshopDay1.csv', header=TRUE)
 
 # What does R interpret this as? use class()
 
-class(data)
+class(spider)
 
 # Good! R interprets it as a data frame
 
 # Look at the dimensions - rows by cols
 
-dim(data)
+dim(spider)
 
 # Look at the first rows with head()
 
-head(data)
+head(spider)
 
 # What are the column names?
 
-colnames(data)
+colnames(spider)
 
 # How are the rows, columns labeled?
 
-labels(data)
+labels(spider)
 
 # Summarize your data
 
-summary(data)
+summary(spider)
 
 # R describes data as numerical, factors, and integers
 # Use str(data) to see what it is describing your data
 
-str(data)
+str(spider)
 
 # Change class using as.factor(), as.numeric(), as.integer(), as.character()
 
-data$Plot <- as.factor(data$Plot)
+spider$survey <- as.factor(spider$survey)
 
-str(data)
+str(spider)
 
 
 
@@ -197,50 +192,35 @@ str(data)
 #   Or, data$column to describe columns
 
 # Specific cells
-data[2,5]
+spider[2,5]
 
 # Specific row
-data[2,]
+spider[2,]
 
 # Specific column
-data[,5]
+spider[,5]
 
 # OR, data$column
 
-data$Veg
+spider$island
 
 # OR, data[['column']]
 
-data[['Veg']]
-
-
+spider[['island']]
 
 
 
 
 # ----- Manipulating parts of data tables ------
 
-
-flow <- read.table('RWorkshopDay2.txt',header=T) 
-# an alternative to read.csv if you can produce a text file
-
-head(flow)
-flow$init_g
-
 # Create a vector by calculating
-# This isnt automatically attached to the "flow" data frame
-dissolved <- (flow$init_g - flow$final_g)/(flow$days)
+# This isnt automatically attached to the "spider" data frame
+webs50m <- (spider$tot_webs/spider$length)*50
 
 # To attach, use cbind() to add "disolved" to "flow"
-flow <- cbind(flow,dissolved)
+spider <- cbind(spider,webs50m)
 
 # Make sure the new column is there
-head(flow)
+head(spider)
 
 
-### now it's your turn #### 
-
-#read in spider.csv 
-#explore dataset -what is min, max, average for the total # of webs? 
-#create a vector describing the number of webs per meter
-#make a histogram of the total number of webs
