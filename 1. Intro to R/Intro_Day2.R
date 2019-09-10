7741a0230228b5ebbb91a17e982838e5242cc00e
Haldre Rogers <haldrerogers@haldre-2015.eeob.iastate.edu>
2019-09-10 14:25:04 -0500

    files for class today

--- 1. Intro to R/Intro_Day2.R
+++
@@ -1,7 +0,0 @@
-
-### now it's your turn #### 
-
-#read in spider.csv 
-#explore dataset -what is min, max, average for the total # of webs? 
-#create a vector describing the number of webs per meter
-#make a histogram of the total number of webs
